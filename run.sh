9d4deab2409e4f3bd859d3077cff13996608f055
Ojvar <MOjvar.AmirHossein@gmail.com>
2022-03-03 13:44:57 +0330

    Update docker-compose

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
+# Create data directories
 mkdir data-0{1,2,3}
 
+# Create key file
+mkdir ./keys
+openssl rand -base64 756 > ./keys/mongo-key.pem
+#chmod 600 ./keys/mongo-key.pem
+
+# Run docker compose
 docker-compose up
