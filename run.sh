5320e8e2d8ad4793c708d5819424fc9861e9382b
AmirHossein MOjvar <MOjvar.AmirHossein@Gmail.com>
2022-03-04 21:16:30 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ mkdir data-0{1,2,3}
 # Create key file
 mkdir ./keys
 openssl rand -base64 756 > ./keys/mongo-key.pem
-#chmod 600 ./keys/mongo-key.pem
+#chmod 400 ./keys/mongo-key.pem
 
 # Run docker compose
 docker-compose up
